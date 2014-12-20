ede095d7c38b2cca20ca99644ed101274a01fbd8
swift <swift@e2r2p18.42.fr>
2014-12-20 14:32:47 +0100

    envoie MAJ

--- myFirstIOSapp/SecondViewController.swift
+++ myFirstIOSapp/SecondViewController.swift
@@ -7,12 +7,20 @@
 //
 
 import UIKit
+import MapKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet weak var mapview: MKMapView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        mapview.showsUserLocation = true
+        mapview.delegate = self
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
