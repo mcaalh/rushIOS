a05aabc1730c9dfc5bd22dbb7b1821a79bfb5f73
Mick Ravoavy <mcaalh@yahoo.fr>
2014-12-21 03:34:44 +0100

    envoie avec logo

--- myFirstIOSapp/FirstViewController.swift
+++ myFirstIOSapp/FirstViewController.swift
@@ -29,7 +29,7 @@ class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDat
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         var cell:UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
         
-        cell.textLabel.text = self.items[indexPath.row]
+        cell.textLabel?.text = self.items[indexPath.row]
         
         return cell
     }
